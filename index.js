bb7d62f440e89f2b951c398bae30c118a2726dad
Shruti Sinha <shruti2809@gmail.com>
2016-06-28 15:21:36 +0530

    REply to hello

--- index.js
+++ index.js
@@ -1,95 +1,84 @@
-'use strict'
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+var express = require('express')
+var bodeParser = require('body-parser')
+var request = reqire('request')
+var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-// Process application/x-www-form-urlencoded
+//Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Process application/json
+//Process application/json
 app.use(bodyParser.json())
 
-// Index route
+//Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+  res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
+//for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+})
+
+//Spin up the server
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
+})
+
+//API End Point - added by Stefan
+app.post('/webhook/', function(req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.text
+    if (event.message && event.messagee.text) {
+      text = event.message.text
+      if (text === 'Hola') {
+        sendGenericMessage(sender)
+        continue
+      }
+      sendTextMessage(sender, "Parrot:
+       " + text.substring(0, 200))
     }
-    res.send('Error, wrong token')
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            if (text === 'Generic') {
-                sendGenericMessage(sender)
-                continue
-            }
-            else if (text === 'Cartoons') {
-                sendCartoonMessage(sender)
-                continue
-            }
-            else if (text === 'hi'|| text === 'Hi'|| text === 'Hello' || text === 'hello' || text === 'Hey') {
-                sendReplyToHiMessage(sender)
-                continue
-            }
-            else if (text === '1. Room in a flat') {
-                sendReplyToRoomMessage(sender)
-                continue
-            }
-            else if (text === '2. Flatmate') {
-                sendReplyToFlatmateMessage(sender)
-                continue
-            }
-            else if (text === '1. One Room' || text === "2. Two Rooms" || text === "3. Three or more Rooms") {
-                sendReplyToNoOfRoomMessage(sender)
-                continue
-            }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
+    if (event.postback) {
+      text = JSON.stringify(event.postback)
+      sentTextMessage(sender, "Postback received: " + text.sbstring(0, 200), token)
+      continue
     }
-    res.sendStatus(200)
+  }
+  res.sendStatus(200)
 })
+var token = "EAAYMofzzI48BANAkz2GeBJHePaxEhraPkjtewNjdZCdiDjGHgUPKZBu9KH5UkCKdrw9ZClvkzjT86V3hf8lfYP9PghLrNSgZBZAxkIaTvZAHo9phzWfHej2yOIRoJzSnVDvR2KvsoH3IhRIZCVyLi0dZAoZCfBEZCuILD71fbNJ8YYRAZDZD"
 
-const token = "EAAYMofzzI48BANAkz2GeBJHePaxEhraPkjtewNjdZCdiDjGHgUPKZBu9KH5UkCKdrw9ZClvkzjT86V3hf8lfYP9PghLrNSgZBZAxkIaTvZAHo9phzWfHej2yOIRoJzSnVDvR2KvsoH3IhRIZCVyLi0dZAoZCfBEZCuILD71fbNJ8YYRAZDZD"
+//function to echo back messages 
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
+  function sendTextMessage(sender, text) {
+    messageData = {
+      text:text
+    }
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
+      url: 'https://graph.facebook.com/v2.6/me/messages'
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: messageData,
+      }
     }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
+      if (error) {
+        console.log("Error sending messages: ", error)
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+      }
     })
-}
+  }
 
-function sendGenericMessage(sender) {
+  //Send a test message back as two cards.
+  function sendGenericMessage(sender) {
     let messageData = {
         "attachment": {
             "type": "template",
@@ -119,7 +108,7 @@ function sendGenericMessage(sender) {
                     }, {
                         "type": "postback",
                         "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
+                        "payload": "Life is quite meaningful.",
                     }],
                 }]
             }
@@ -140,212 +129,4 @@ function sendGenericMessage(sender) {
             console.log('Error: ', response.body.error)
         }
     })
-}
-
-function sendCartoonMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "First element",
-                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c1_zpsmpvokudw.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://garfield.com/",
-                        "title": "Garfield's website URL"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "second element",
-                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c2_zpskdjdv5yx.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/TomandJerry/",
-                        "title": "Jerry's fb page URL"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-// Reply to "hi" or "Hi" or "Hello"
-function sendReplyToHiMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Hi there! I am flatshare Bot. Can you tell me what are you looking for?",
-                    //"subtitle": "",
-                  //"image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c1_zpsmpvokudw.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        //"url": "https://garfield.com/",
-                        "title": "1. Room in a flat",
-                        "payload": "Payload for first element in a generic bubble",
-                    }, {
-                          "type": "postback",
-                          //"url": "https://garfield.com/",
-                          "title": "2. Flatmate",
-                          "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-function sendReplyToRoomMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Thanks, and how many rooms are you looking for? :)",
-                    //"subtitle": "",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "1. One Room",
-                        "payload": "Payload for first element in a generic bubble",
-                    }, {
-                          "type": "postback",
-                          "title": "2. Two Rooms",
-                          "payload": "Payload for second element in a generic bubble",
-                    }, {
-                          "type": "postback",
-                          "title": "3. Three or more Rooms",
-                          "payload": "Payload for third element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-    function sendReplyToFlatmateMessage(sender) {
-        let messageData = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Thanks, and how many flatmates are you looking for? :)",
-                        "buttons": [{
-                            "type": "postback",
-                            "title": "1. One flatmate",
-                            "payload": "Payload for first element in a generic bubble",
-                        }, {
-                              "type": "postback",
-                              "title": "2. Two flatmates",
-                              "payload": "Payload for second element in a generic bubble",
-                        }],
-                    }]
-                }
-            }
-        }
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token:token},
-            method: 'POST',
-            json: {
-                recipient: {id:sender},
-                message: messageData,
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-      })
-}
-// Send Reply To No Of Room Message
-
-function sendReplyToNoOfRoomMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Hi there! I am flatshare Bot. Can you tell me what are you looking for?",
-                    //"subtitle": "",
-                    "buttons": [{
-                        "type": "postback",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-   })
 }
---
+++ index.original.js
@@ -0,0 +1,351 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            else if (text === 'Cartoons') {
+                sendCartoonMessage(sender)
+                continue
+            }
+            else if (text === 'hi'|| text === 'Hi'|| text === 'Hello' || text === 'hello' || text === 'Hey') {
+                sendReplyToHiMessage(sender)
+                continue
+            }
+            else if (text === '1. Room in a flat') {
+                sendReplyToRoomMessage(sender)
+                continue
+            }
+            else if (text === '2. Flatmate') {
+                sendReplyToFlatmateMessage(sender)
+                continue
+            }
+            else if (text === '1. One Room' || text === "2. Two Rooms" || text === "3. Three or more Rooms") {
+                sendReplyToNoOfRoomMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAYMofzzI48BANAkz2GeBJHePaxEhraPkjtewNjdZCdiDjGHgUPKZBu9KH5UkCKdrw9ZClvkzjT86V3hf8lfYP9PghLrNSgZBZAxkIaTvZAHo9phzWfHej2yOIRoJzSnVDvR2KvsoH3IhRIZCVyLi0dZAoZCfBEZCuILD71fbNJ8YYRAZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "First element",
+                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/d2_zps5k4xcryx.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "Messenger URL"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "second element",
+                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/d1_zps2gztnqo1.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "Messenger URL"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function sendCartoonMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "First element",
+                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c1_zpsmpvokudw.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://garfield.com/",
+                        "title": "Garfield's website URL"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "second element",
+                    "image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c2_zpskdjdv5yx.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.facebook.com/TomandJerry/",
+                        "title": "Jerry's fb page URL"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+// Reply to "hi" or "Hi" or "Hello"
+function sendReplyToHiMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Hi there! I am flatshare Bot. Can you tell me what are you looking for?",
+                    //"subtitle": "",
+                  //"image_url": "http://i280.photobucket.com/albums/kk176/shruti28009/c1_zpsmpvokudw.jpg",
+                    "buttons": [{
+                        "type": "postback",
+                        //"url": "https://garfield.com/",
+                        "title": "1. Room in a flat",
+                        "payload": "Payload for first element in a generic bubble",
+                    }, {
+                          "type": "postback",
+                          //"url": "https://garfield.com/",
+                          "title": "2. Flatmate",
+                          "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+function sendReplyToRoomMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Thanks, and how many rooms are you looking for? :)",
+                    //"subtitle": "",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "1. One Room",
+                        "payload": "Payload for first element in a generic bubble",
+                    }, {
+                          "type": "postback",
+                          "title": "2. Two Rooms",
+                          "payload": "Payload for second element in a generic bubble",
+                    }, {
+                          "type": "postback",
+                          "title": "3. Three or more Rooms",
+                          "payload": "Payload for third element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+    function sendReplyToFlatmateMessage(sender) {
+        let messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Thanks, and how many flatmates are you looking for? :)",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "1. One flatmate",
+                            "payload": "Payload for first element in a generic bubble",
+                        }, {
+                              "type": "postback",
+                              "title": "2. Two flatmates",
+                              "payload": "Payload for second element in a generic bubble",
+                        }],
+                    }]
+                }
+            }
+        }
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:token},
+            method: 'POST',
+            json: {
+                recipient: {id:sender},
+                message: messageData,
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+      })
+}
+// Send Reply To No Of Room Message
+
+function sendReplyToNoOfRoomMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Thanks, and your area would be?",
+                    //"subtitle": "",
+                    "buttons": [{
+                        "type": "postback",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+   })
+}
